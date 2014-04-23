f5313c5b548a3728ebd514d93c1a21afcd9f4bd8
megazear7 <alexlockhart7@gmail.com>
2014-04-23 12:58:35 -0400

    please please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Filecabinet::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
   # config.assets.precompile += [ 'application.css.scss' ]
 
   # Ignore bad email addresses and do not raise email delivery errors.
